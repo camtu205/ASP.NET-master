32316eb8f42a9db0eb447fe21d3cf7636e57a025
camtu205 <nguyenthicamtu830@gmail.com>
2026-04-19 22:00:58 +0700

    Auto-add ServiceIds column to Treatments table to fix 500 error

--- NguyenThiCamTu_2123110472/Program.cs
+++ NguyenThiCamTu_2123110472/Program.cs
@@ -144,6 +144,7 @@ using (var scope = app.Services.CreateScope())
         try {
             context.Database.ExecuteSqlRaw("ALTER TABLE \"Promotions\" ADD COLUMN IF NOT EXISTS \"MaxUsage\" INTEGER;");
             context.Database.ExecuteSqlRaw("ALTER TABLE \"Promotions\" ADD COLUMN IF NOT EXISTS \"ApplicableServiceIds\" TEXT;");
+            context.Database.ExecuteSqlRaw("ALTER TABLE \"Treatments\" ADD COLUMN IF NOT EXISTS \"ServiceIds\" TEXT;");
         } catch { /* Bỏ qua nếu đã có hoặc lỗi syntax (DB khác Postgres) */ }
 
         app.Logger.LogInformation(">>> DATABASE MIGRATION SUCCESSFUL! <<<");
