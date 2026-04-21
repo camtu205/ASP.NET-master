6d646d7eb99217eae17957c5e57e0619ba6cfa62
camtu205 <nguyenthicamtu830@gmail.com>
2026-04-21 23:44:20 +0700

    Auto-initialize database schema on startup to fix 500 error on registration

--- NguyenThiCamTu_2123110472/Program.cs
+++ NguyenThiCamTu_2123110472/Program.cs
@@ -109,6 +109,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+// Tự động cập nhật Database cấu trúc mới nhất
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
 app.UseForwardedHeaders();
 
 // Enable Static Files and Default Files (index.html)
