94932f1c3d68da9c94ad652d0c292ec428b6435c
camtu205 <nguyenthicamtu830@gmail.com>
2026-04-23 10:10:58 +0700

    feat: add migration for ImageUrl

---
+++ NguyenThiCamTu_2123110472/Migrations/20260423000000_AddImageUrl.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NguyenThiCamTu_2123110472.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Services",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Services");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Products");
+        }
+    }
+}
